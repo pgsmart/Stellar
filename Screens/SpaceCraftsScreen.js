fe07860fd8f6e1a162ade7973faaff989ae7f835
pgsmart <101372312+pgsmart@users.noreply.github.com>
2023-07-24 17:19:13 -0500

    Add files via upload

--- Screens/SpaceCraftsScreen.js
+++ Screens/SpaceCraftsScreen.js
@@ -5,47 +5,14 @@ import MapView from 'react-native-maps';
 
 export default class SpaceCraftsScreen extends Component {
 
-    constructor(){
-        super();
-        this.state = {
-            storage: ''
-        }
-    }
-
-    componentDidMount=()=>{
-        this.getIssLocation()
-    }
-
-    getIssLocation=()=>{
-        axios.get('https://api.wheretheiss.at/v1/satellites/25544')
-        .then((response)=>{this.setState({storage: response.data})})
-        .catch(()=>{Alert.alert("Error")})
-    }
-
+   
     render() {
 
-        console.log(this.state.storage)
 
         return (
             <View style={styles.container}>            
                 <Text>Space Crafts Screen</Text>
-            <View style={{flex:0.5,borderWidth:1}}>
-                <MapView
-                initialRegion={{
-                    latitude: this.state.storage.latitude,
-                    longitude: this.state.storage.longitude,
-                    latitudeDelta: 100,
-                    longitudeDelta: 100
-                }}>
-                </MapView>
-            </View>
-                <View>
-                <Text>Latitude: {this.state.storage.latitude}</Text>
-                <Text>Longitude: {this.state.storage.longitude}</Text>
-                <Text>Name: {this.state.storage.name}</Text>
-                <Text>Velocity: {this.state.storage.velocity}</Text>
-                </View>
-               
+          
             </View>
         )
     }
--- Screens/StarMapScreen.js
+++ Screens/StarMapScreen.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View ,SafeAreaView,StyleSheet,TextInput} from 'react-native';
+import {WebView} from 'react-native-webview'
+import CheckBox from 'expo-checkbox';
+
 export default class StarMapScreen extends Component {
+    constructor(){
+        super();
+        this.state={
+            longitude: 77.102493,
+            latitude: 28.704060,
+            constellations: true,
+            gridLines: true
+        }
+    }
+
+
     render() {
+        
         return (
-            <View>
-                         
-                <Text>Star Map Screen</Text>
+            <View style={{flex:1}}>
+                <Text style={{fontSize:30}}>Star Map Screen</Text>
+                <TextInput  style={{marginLeft:10,marginTop:10}} placeholder='Enter Longitude...' onChangeText={(text)=>{
+                    this.setState({longitude: text})
+                }}></TextInput>
+                <TextInput style={{marginLeft:10,marginTop:10}} placeholder='Enter Latitude...' onChangeText={(text)=>{
+                    this.setState({latitude: text})
+                }}></TextInput>
+                <View style={{flexDirection: 'row',width:'50%',}}>
+                <CheckBox style={{marginLeft:10,marginTop:10}}value={this.state.constellations} onValueChange={(value)=>{this.setState({constellations: value})}}/>
+                <Text style={{marginLeft:10,marginTop:10}}>Show Constellations</Text>
+                </View>
+                <View style={{flexDirection: 'row',width:'50%',}}>
+                    <CheckBox style={{marginLeft:10,marginTop:10}} value={this.state.gridLines} onValueChange={(value)=>{this.setState({gridLines: value})}}/>
+                    <Text style={{marginLeft: 10,marginTop:10}}>Show GridLines</Text>
+                </View>
+                                
+                <WebView 
+                    style={styles.mapView}
+                    source={{uri: 'https://virtualsky.lco.global/embed/index.html?longitude='+ this.state.longitude + '&latitude=' + this.state.latitude + '&constellations='+this.state.constellations+'&constellationlabels=true&showstarlabels=true&gridlines_az='+this.state.gridLines+'&live=true'}}
+                />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    mapView:{
+        marginTop: 50,
+        width:'100%'
+    }
+})
